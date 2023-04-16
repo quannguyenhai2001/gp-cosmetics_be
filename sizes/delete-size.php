7979fa7f4cfe66673e37eb8f86b50ea8197e6817
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-04-16 17:18:18 +0700

    feat: add delete size api

---
+++ sizes/delete-size.php
@@ -0,0 +1,42 @@
+<?php
+//add headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: DELETE");
+
+//import file
+include_once "../database/database.php";
+include_once "../middleware/check-auth.php";
+//initialize database
+$obj = new Database();
+
+//check method request
+if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+    $payload = checkAuth(getallheaders(), "admin");
+    if ($payload) {
+        $data = json_decode(file_get_contents("php://input", true));
+        $ids = $data->ids;
+        $string = '(' . implode(',', $ids) . ')';
+        $sql = $obj->delete("sizes", "`sizes`.`id` IN  $string");
+        $result = $obj->getResult();
+        if ($sql) {
+            http_response_code(200);
+            echo json_encode([
+                "status" => "success",
+                "message" => "Delete size success!"
+            ]);
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                "status" => "error",
+                "message" => $result,
+            ]);
+        }
+    }
+} else {
+    echo json_encode(array(
+        "status" => "error",
+        "message" => "Access denied!",
+    ));
+}
