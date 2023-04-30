b269e70feaaf7c0c6c5c8bb58ddd52e2f5a39ad5
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-04-30 23:09:23 +0700

    fix: edit response for get all ratings api

--- ratings/get-all-ratings.php
+++ ratings/get-all-ratings.php
@@ -13,14 +13,38 @@ $obj = new Database();
 
 //check method request
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    $product_id = $_GET['product_id'];
-    $sql = $obj->select("ratings", "ratings.*, users.`display_name`, users.`avatar`", "users", "ratings.`user_id` = users.`id`", "ratings.`product_id` = '$product_id'", "", "");
+    $pagination = null;
+    $limit = 15;
+
+    if (isset($_GET['use_page']) && $_GET['use_page'] == 1) {
+        $offsetIndex = isset($_GET['page']) ? ($limit * floatval($_GET['page'])) - $limit : 0;
+        $pagination = $limit . " OFFSET " . $offsetIndex;
+    }
+
+    $conditionString = "";
+    if (isset($_GET['product_id'])) {
+        $conditionString  = $conditionString . "product_id = " . $_GET['product_id'] . " and ";
+    }
+    $conditionString =  rtrim($conditionString, " and ");
+
+    $sql = $obj->select("ratings", "ratings.*, users.`username`, users.`avatar`, products.name as product_name", "users JOIN products", "ratings.`user_id` = users.`id` and ratings.`product_id` = products.`id`", $conditionString, "",  $pagination);
     $result = $obj->getResult();
     if ($sql) {
+        $pageInfo = array();
+        $total = $obj->getResult($obj->select("ratings", "COUNT(*)", "users", "ratings.`user_id` = users.`id`", $conditionString, "",  ""));
+
+        $pageInfo["total"] = floatval($total[0]["COUNT(*)"]);
+        if (isset($_GET['use_page']) && $_GET['use_page'] == 1) {
+            $pageInfo["page"] = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+            $pageInfo["limit"] = $limit;
+            $pageInfo["total_page"] = ceil($total[0]["COUNT(*)"] / $limit);
+        }
+
         http_response_code(200);
         echo json_encode(array(
             "status" => "success",
             "data" => $result,
+            "pageInfo" =>  $pageInfo,
         ));
     } else {
         http_response_code(400);
