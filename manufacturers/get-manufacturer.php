82d061cea5e0155fee58f1866447718eebe8f992
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-05-01 16:00:54 +0700

    feat: get manu api

---
+++ manufacturers/get-manufacturer.php
@@ -0,0 +1,40 @@
+<?php
+//add headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: *");
+
+//import file
+include_once "../middleware/check-auth.php";
+include_once("../database/database.php");
+include_once("../vendor/autoload.php");
+
+$obj = new Database();
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $payload = checkAuth(getallheaders(), null);
+    if ($payload) {
+        $manufacturer_id = $_GET['manufacturer_id'];
+        $sql = $obj->select("manufacturers", "*", null, null, "id='$manufacturer_id'", null, null);
+        $result = $obj->getResult();
+        if ($sql) {
+            http_response_code(200);
+            echo json_encode([
+                "status" => "success",
+                "data" => $result[0],
+            ]);
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                "status" => "error",
+                "message" => $result,
+            ]);
+        }
+    }
+} else {
+    echo json_encode(array(
+        "status" => "error",
+        "message" => "Access denied!"
+    ));
+}
