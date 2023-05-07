e3cc1e610f65b135588224db71d5c6169ddb1241
quannguyenhai2001 <quanbn134679258@gmail.com>
2023-05-07 18:16:43 +0700

    feat: add total records api and get recent transactions api

--- statistical/get-recent-transactions.php
+++ statistical/get-recent-transactions.php
@@ -13,15 +13,16 @@ $obj = new Database();
 
 //check method request
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    $sql = "SELECT YEAR(bills.create_at) as year, MONTH(bills.create_at) as month, SUM(bill_details.quantity * ((bill_details.product_price + bill_details.size_additional_price) * (1 - bill_details.product_promotion / 100))) as revenue
+    $sql = "SELECT bills.*, users.username , SUM(bill_details.quantity * ((bill_details.product_price + bill_details.size_additional_price) * (1 - bill_details.product_promotion / 100))) as total_price
     FROM bills
     JOIN bill_details ON bills.id = bill_details.bill_id
-    JOIN products ON bill_details.product_id = products.id
-    JOIN sizes ON bill_details.size_id = sizes.id
+    JOIN users ON bills.user_id = users.id
     WHERE bills.status = 'Đã giao'
-    GROUP BY YEAR(bills.create_at), MONTH(bills.create_at)
-     
-    ORDER BY YEAR(bills.create_at), MONTH(bills.create_at)";
+    GROUP BY 
+    bills.id
+    ORDER BY 
+    bills.create_at DESC 
+    LIMIT 6";
     $query = $obj->getConnection()->query($sql);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     if ($query) {
---
+++ statistical/total-records.php
@@ -0,0 +1,77 @@
+<?php
+//add headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+
+//import file
+include_once "../database/database.php";
+
+//initialize database
+$obj = new Database();
+
+//check method request
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    //users
+    $sql = "SELECT COUNT(*) AS total_users FROM users;";
+    $sql1 = "SELECT COUNT(*) AS new_users_last_month FROM users WHERE create_at >= DATE_SUB(NOW(), INTERVAL 1 MONTH)";;
+    $query = $obj->getConnection()->query($sql);
+    $query1 = $obj->getConnection()->query($sql1);
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $result1 = $query1->fetchAll(PDO::FETCH_ASSOC);
+
+    //bills
+    $sql2 = "SELECT COUNT(*) AS total_orders FROM bills WHERE bills.status = 'Đã giao'";
+    $sql3 = "SELECT COUNT(*) AS total_orders_last_month FROM bills WHERE create_at BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";;
+    $query2 = $obj->getConnection()->query($sql2);
+    $query3 = $obj->getConnection()->query($sql3);
+    $result2 = $query2->fetchAll(PDO::FETCH_ASSOC);
+    $result3 = $query3->fetchAll(PDO::FETCH_ASSOC);
+
+    //revenue
+    $sql4 = "SELECT  SUM(bill_details.quantity * ((bill_details.product_price + bill_details.size_additional_price) * (1 - bill_details.product_promotion / 100))) as total_revenue FROM bills JOIN bill_details ON bills.id = bill_details.bill_id WHERE status = 'Đã giao'";
+    $sql5 = "SELECT 
+    SUM(bill_details.quantity * ((bill_details.product_price + bill_details.size_additional_price) * (1 - bill_details.product_promotion / 100))) as total_revenue, 
+    MONTH(bills.create_at) AS month, 
+    YEAR(bills.create_at) AS year
+    FROM 
+    bills
+    JOIN bill_details ON bills.id = bill_details.bill_id
+    WHERE 
+    status = 'Đã giao' AND
+    bills.create_at BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()
+    GROUP BY 
+    MONTH(bills.create_at), YEAR(bills.create_at)";;
+    $query4 = $obj->getConnection()->query($sql4);
+    $query5 = $obj->getConnection()->query($sql5);
+    $result4 = $query4->fetchAll(PDO::FETCH_ASSOC);
+    $result5 = $query5->fetchAll(PDO::FETCH_ASSOC);
+    if ($query) {
+        $arrayValue = array();
+        $arrayValue["users"]['total_users'] = $result[0]['total_users'];
+        $arrayValue["users"]['new_users_last_month'] = $result1[0]['new_users_last_month'];
+        $arrayValue["bills"]['total_orders'] = $result2[0]['total_orders'];
+        $arrayValue["bills"]['total_orders_last_month'] = $result3[0]['total_orders_last_month'];
+        $arrayValue["revenue"]['total_revenue'] = $result4[0]['total_revenue'];
+        $arrayValue["revenue"]['total_revenue_last_two_month'] = $result5;
+        http_response_code(200);
+        echo json_encode(
+            [
+                "status" => "success",
+                "data" => $arrayValue,
+            ]
+        );
+    } else {
+        http_response_code(400);
+        echo json_encode([
+            "status" => "error",
+            "message" => $result,
+        ]);
+    }
+} else {
+    echo json_encode(array(
+        "status" => "error",
+        "message" => "Access denied!",
+    ));
+}
